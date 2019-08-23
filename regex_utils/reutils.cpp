c53042ebc457675b0728304a4762630043a8ca19
Drulia-san <admin@drulia.ru>
2019-08-24 00:48:37 +0300

    v1.03

--- regex_utils/reutils.cpp
+++ regex_utils/reutils.cpp
@@ -2,8 +2,72 @@
 #include <regex>
 #include <stdexcept>
 
-namespace ReUtils
+namespace reu
 {
+	struct rf_t
+	{
+		std::string re;
+		std::regex::flag_type flags;
+	};
+
+	std::string IndexSubstr(const std::string& str, size_t begin, size_t end)
+	{
+		return str.substr(begin, end - begin + 1);
+	}
+
+	rf_t SeparateFlags(const std::string re)
+	{
+		size_t i = re.find('/');
+		if (i == std::string::npos || (i > 0 && re[i - 1] == '\\'))
+			return { re, static_cast<std::regex::flag_type>(0) };
+
+		rf_t rf;
+		std::string flags = IndexSubstr(re, i+1, re.length() - 1);
+		rf.re = re.substr(0, i);
+		rf.flags = static_cast<std::regex::flag_type>(0);
+
+		for (auto c : flags)
+		{
+			switch (c)
+			{
+			case 'E':
+				rf.flags |= std::regex_constants::ECMAScript;
+				break;
+			case 'r':
+				rf.flags |= std::regex_constants::grep;
+				break;
+			case 'i':
+				rf.flags |= std::regex_constants::icase;
+				break;
+			case 'x':
+				rf.flags |= std::regex_constants::extended;
+				break;
+			case 'a':
+				rf.flags |= std::regex_constants::awk;
+				break;
+			case 'b':
+				rf.flags |= std::regex_constants::basic;
+				break;
+			case 'e':
+				rf.flags |= std::regex_constants::egrep;
+				break;
+			case 'c':
+				rf.flags |= std::regex_constants::collate;
+				break;
+			case 'o':
+				rf.flags |= std::regex_constants::optimize;
+				break;
+			case 'n':
+				rf.flags |= std::regex_constants::nosubs;
+				break;
+			default:
+				continue;
+			}
+		}
+
+		return rf;
+	}
+
 	void ReplaceStrIndexRange(std::string& str, const std::string& replacingStr, size_t begin, size_t end)
 	{
 		size_t len = str.length();
@@ -31,13 +95,8 @@ namespace ReUtils
 
 		std::string pre, post;
 		pre = str.substr(0, begin);
-		post = str.substr(end, len - end + 1);
+		post = str.substr(end + 1, len - end);
 		str = pre + replacingStr + post;
-	}
-
-	std::string IndexSubstr(const std::string& str, size_t begin, size_t end)
-	{
-		return str.substr(begin, end - begin + 1);
 	}
 
 	void PatternReplace(std::string& str, size_t begin, size_t end, const std::string pattern, const std::vector<std::string>& groups)
@@ -61,7 +120,7 @@ namespace ReUtils
 			size_t u = 0;
 			size_t t(1);
 
-			for (int i = strlen(a) - 1; i >= 0; --i)
+			for (size_t i = strlen(a) - 1; i >= 0; --i)
 			{
 				if (a[i] >= '0' && a[i] <= '9')
 				{
@@ -128,8 +187,8 @@ namespace ReUtils
 		ReplaceStrIndexRange(str, insertion, begin, end);
 	}
 
-	//match
-	match::match(std::string& str, size_t begin, size_t end, const std::vector<std::string>& groups, const std::vector<grp_range>& granges)
+	//match_t
+	match_t::match_t(std::string& str, size_t begin, size_t end, const std::vector<std::string>& groups, const std::vector<grp_range_t>& granges)
 		: _str(&str)
 		, _begin(begin)
 		, _end(end)
@@ -138,7 +197,7 @@ namespace ReUtils
 	{
 	}
 
-	match::match(const match& copy)
+	match_t::match_t(const match_t& copy)
 		: _str(copy._str)
 		, _begin(copy._begin)
 		, _end(copy._end)
@@ -147,7 +206,7 @@ namespace ReUtils
 	{
 	}
 
-	match& match::operator=(const match& copy)
+	match_t& match_t::operator=(const match_t& copy)
 	{
 		_str = copy._str;
 		_begin = copy._begin;
@@ -157,28 +216,28 @@ namespace ReUtils
 		return *this;
 	}
 
-	void match::Replace(const std::string& pattern)
+	void match_t::Replace(const std::string& pattern)
 	{
 		if (IsMatching())
 			PatternReplace( *_str, _begin, _end, pattern, _groups );
 	}
 
-	std::string match::operator[](size_t i) const
+	std::string match_t::operator[](size_t i) const
 	{
 		return GetGroup(i);
 	}
 
-	size_t match::Begin() const
+	size_t match_t::Begin() const
 	{
 		return _begin;
 	}
 
-	size_t match::End() const
+	size_t match_t::End() const
 	{
 		return _end;
 	}
 
-	std::string match::GetMatchingString() const
+	std::string match_t::GetMatchingString() const
 	{
 		if (!IsMatching())
 			return "";
@@ -186,7 +245,7 @@ namespace ReUtils
 		return IndexSubstr(*_str, _begin, _end);
 	}
 
-	std::string match::GetGroup(size_t n) const
+	std::string match_t::GetGroup(size_t n) const
 	{
 		if (n == 0)
 			return GetMatchingString();
@@ -195,7 +254,7 @@ namespace ReUtils
 		return _groups[n-1];
 	}
 
-	match::grp_range match::GetGroupRange(size_t n)
+	match_t::grp_range_t match_t::GetGroupRange(size_t n)
 	{
 		if (n == 0)
 			return { _begin, _end };
@@ -206,60 +265,70 @@ namespace ReUtils
 		return _granges[n-1];
 	}
 
-	bool match::IsMatching() const
+	bool match_t::IsMatching() const
 	{
 		return (_begin != std::string::npos);
 	}
 
-	//matches
-	matches::matches()
+	//matches_t
+	matches_t::matches_t()
 	{
 	}
 
-	matches::matches(const matches& copy)
+	matches_t::matches_t(const matches_t& copy)
 		: _mvec(copy._mvec)
 	{
 	}
 
-	matches& matches::operator=(const matches& copy)
+	matches_t& matches_t::operator=(const matches_t& copy)
 	{
 		_mvec = copy._mvec;
 		return *this;
 	}
 
-	matches& matches::operator+=(const match& m)
+	matches_t& matches_t::operator+=(const match_t& m)
 	{
 		_mvec.push_back(m);
 		return *this;
 	}
 
-	match& matches::operator[](size_t i)
+	match_t& matches_t::operator[](size_t i)
 	{
 		return _mvec.at(i);
 	}
 
-	void matches::AddMatch(const match& m)
+	void matches_t::AddMatch(const match_t& m)
 	{
 		_mvec.push_back(m);
 	}
 
-	void matches::ReplaceAll(const std::string& pattern)
+	void matches_t::ReplaceAll(const std::string& pattern)
 	{
 		for (auto& m : _mvec)
 			m.Replace(pattern);
 	}
 
-	match matches::Get(size_t i) const
+	std::vector<match_t>::iterator matches_t::begin()
+	{
+		return _mvec.begin();
+	}
+
+	std::vector<match_t>::iterator matches_t::end()
+	{
+		return _mvec.end();
+	}
+
+	match_t matches_t::Get(size_t i) const
 	{
 		return _mvec.at(i);
 	}
 
-	size_t matches::Count() const
+	size_t matches_t::Count() const
 	{
 		return _mvec.size();
 	}
 
-	bool matches::HasMatches() const
+	bool matches_t::HasMatches() const
 	{
 		if (_mvec.size() == 0)
 			return false;
@@ -267,23 +336,26 @@ namespace ReUtils
 	}
 
 	//Utils
-	match Search(std::string& str, const std::string re, size_t offset)
+	match_t Search(std::string& str, const std::string re, size_t offset)
 	{
 		std::string a;
-		std::regex exp(re);
+		rf_t rf = SeparateFlags(re);
+		std::regex exp(rf.re, rf.flags);
 		std::smatch sm;
 		std::vector<std::string> groups;
-		std::vector<match::grp_range> ranges;
+		std::vector<match_t::grp_range_t> ranges;
 		size_t begin = std::string::npos;
 		size_t end = std::string::npos;
 		std::string offstr = "";
 		std::string* pstr = &str;
+		
+		
 
-		auto countGroupRange = [](const std::smatch::value_type& g, const std::string& str, size_t offset) -> match::grp_range
+		auto countGroupRange = [](const std::smatch::value_type& g, const std::string& str, size_t offset) -> match_t::grp_range_t
 		{
 			size_t i = 0;
 			int n = 0;
-			match::grp_range range = { std::string::npos, std::string::npos };
+			match_t::grp_range_t range = { std::string::npos, std::string::npos };
 			for (auto it = str.begin(); it != str.end(); it++)
 			{
 				if (it == g.first)
@@ -305,6 +377,8 @@ namespace ReUtils
 
 			if (range.begin != std::string::npos && range.end == std::string::npos)
 				range.end = str.length() - 1;
+			else
+				range.end--;
 
 			if (offset > 0)
 			{
@@ -322,7 +396,7 @@ namespace ReUtils
 		}
 
 		if (offset >= str.length())
-			return match(str, begin, end, groups, ranges);
+			return match_t(str, begin, end, groups, ranges);
 
 		if (std::regex_search(*pstr, sm, exp)) 
 		{
@@ -343,13 +417,13 @@ namespace ReUtils
 
 		}
 
-		return match(str, begin, end, groups, ranges);
+		return match_t(str, begin, end, groups, ranges);
 	}
 
-	matches SearchAll(std::string& str, const std::string re, size_t offset)
+	matches_t SearchAll(std::string& str, const std::string re, size_t offset)
 	{
-		matches ms;
-		match m = Search(str, re, offset);
+		matches_t ms;
+		match_t m = Search(str, re, offset);
 
 		if (!m.IsMatching())
 			return ms;
@@ -366,14 +440,16 @@ namespace ReUtils
 
 	bool IsMatching(const std::string& str, std::string re)
 	{
-		return std::regex_match(str, std::regex(re));
+		rf_t rf = SeparateFlags(re);
+		std::regex exp(rf.re, rf.flags);
+		return std::regex_match(str, exp);
 	}
 
 	size_t ReplaceAll(std::string& str, const std::string& re, const std::string& pattern)
 	{
 		size_t off = 0;
 		size_t replaces = 0;
-		match m = Search(str, re, off);
+		match_t m = Search(str, re, off);
 
 		while (m.IsMatching())
 		{
@@ -386,4 +462,4 @@ namespace ReUtils
 		return replaces;
 	}
 
-} //namespace ReUtils
+} //namespace reu
--- regex_utils/reutils.h
+++ regex_utils/reutils.h
@@ -3,15 +3,15 @@
 #include <string>
 #include <vector>
 
-namespace ReUtils
+namespace reu
 {
 	void ReplaceStrIndexRange(std::string& str, const std::string& replacingStr, size_t begin, size_t end);
 	std::string IndexSubstr(const std::string& str, size_t begin, size_t end);
 
-	class match
+	class match_t
 	{
 	public:
-		struct grp_range
+		struct grp_range_t
 		{
 			size_t begin;
 			size_t end;
@@ -21,17 +21,17 @@ namespace ReUtils
 		size_t _begin;
 		size_t _end;
 		std::vector<std::string> _groups;
-		std::vector<grp_range> _granges;
+		std::vector<grp_range_t> _granges;
 		std::string* _str;
 
-		match() = delete;
+		match_t() = delete;
 
 	public:
-		match(std::string& str, size_t begin, size_t end,
+		match_t(std::string& str, size_t begin, size_t end,
 			const std::vector<std::string>& groups, 
-			const std::vector<grp_range>& granges);
-		match(const match& copy);
-		match& operator=(const match& copy);
+			const std::vector<grp_range_t>& granges);
+		match_t(const match_t& copy);
+		match_t& operator=(const match_t& copy);
 
 		void Replace(const std::string& pattern);
 
@@ -40,32 +40,39 @@ namespace ReUtils
 		size_t End() const;
 		std::string GetMatchingString() const;
 		std::string GetGroup(size_t n) const;
-		grp_range GetGroupRange(size_t n);
+		grp_range_t GetGroupRange(size_t n);
 		bool IsMatching() const;
 	};
 
-	class matches
+	class matches_t
 	{
+		friend matches_t SearchAll(std::string& str, const std::string re, size_t offset);
+
 	private:
-		std::vector<match> _mvec;
+		std::vector<match_t> _mvec;
+
+	protected:
+		void AddMatch(const match_t& m);
+		matches_t& operator+=(const match_t& m);
 
 	public:
-		matches();
-		matches(const matches& copy);
-		matches& operator=(const matches& copy);
-		matches& operator+=(const match& m);
-		match& operator[](size_t i);
+		matches_t();
+		matches_t(const matches_t& copy);
+		matches_t& operator=(const matches_t& copy);
+		match_t& operator[](size_t i);
 
-		void AddMatch(const match& m);
 		void ReplaceAll(const std::string& pattern);
 
-		match Get(size_t i) const;
+		std::vector<match_t>::iterator begin();
+		std::vector<match_t>::iterator end();
+
+		match_t Get(size_t i) const;
 		size_t Count() const;
 		bool HasMatches() const;
 	};
 
-	match Search(std::string& str, const std::string re, size_t offset = 0);
-	matches SearchAll(std::string& str, const std::string re, size_t offset = 0);
+	match_t Search(std::string& str, const std::string re, size_t offset = 0);
+	matches_t SearchAll(std::string& str, const std::string re, size_t offset = 0);
 	bool IsMatching(const std::string& str, std::string re);
 	size_t ReplaceAll(std::string& str, const std::string& re, const std::string& pattern);
 }
